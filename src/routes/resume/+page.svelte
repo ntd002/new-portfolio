69868c83ea22fb27f3aadfcccd03e5e9f545031b
ntd002 <ntd002@ucsd.edu>
2024-10-23 15:19:43 -0700

    Fixed disappearances

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -2,70 +2,58 @@
     <title>Resume</title>
 </svelte:head>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resume</title>
-    <link rel="stylesheet" type="text/css" href="https://ntd002.github.io/portfolio/style.css">
-    <link rel="stylesheet" type="module" href="https://ntd002.github.io/portfolio/global.js">
-</head>
-<body>
-    <h1>Resume</h1>
+<h1>Resume</h1>
 
-    <div class="resumes">
-        <article>
-            <h2>Education</h2>
-            <p>
-                <a class="resuLink" href="https://omds.ucsd.edu/" target="_blank"><b>University of California San Diego, Halicioğlu Data Science Institute</b></a><br><br>
-                &#x2022;Master of Data Science Online<br>
-                &#x2022;GPA 3.783<br>
-                &#x2022;Expected <time datetime="2024-03">March 2024</time>
-            </p>
-            <p><a class="resuLink" href="https://dornsife.usc.edu/cognitive-science/" target="_blank"><b>University of Southern California, Dornsife College of Letters, Arts and Sciences</b></a><br><br>
-                &#x2022;Bachelor of Arts in Cognitive Science<br>
-                &#x2022;Minor in Computer Programming<br>
-                &#x2022;GPA 3.45<br>
-                &#x2022;Graduated <time datetime="2022-05">May 2022</time></p>
-        </article>
-        <article>
-            <h2>Employment</h2>
-            <p>
-                <b><a class="resuLink" href="https://youtu.be/dQw4w9WgXcQ?si=yFpgaZFQPK23v-I4" target="_blank">Department of Defense, Data Analyst</b><br><br>
-                &#x2022;Collect and organize data from various sources and mediums<br>
-                &#x2022;Interact with stakeholders and present deliverables with clarity and brevity<br>
-                &#x2022;Allocate time and effort spent supporting different programs<br>
-                &#x2022;Employed since <time datetime="2022-09">September 2022</time>
-            </p>
-            <p>
-                <b><a class="resuLink" href="https://usctrojans.com/sports/womens-tennis" target="_blank">USC Athletics, Tennis Team Manager-USC Women's Tennis</b><br><br>
-                &#x2022;Communicate with the team to maintain a precise record of materials used<br>
-                &#x2022;Apply critical thinking and problem solving to overcome issues with varying product makeup<br>
-                &#x2022;Anticipate problems and communicate with the appropriate team member to deal with them<br>
-                &#x2022;Employed between <time datetime="2019-08">August 2019</time> - <time datetime="2022-05">May 2022</time>
-            </p>
-        </article>
-        <article>
-            <h2>Skills</h2>
-            <p>
-                &#x2022;<b>Languages:</b> Fluent in English; Conversational Proficiency in Spanish<br><br>
-                &#x2022;<b>Computer Skills:</b> Skilled in the fundamentals and the data structures of C++, Built web applications through HTML, CSS, and JavaScript with a database interface through PHP
-            </p>
-        </article>
-        <article>
-            <h2>Hobbies</h2>
-            <p>
-                <b>Video Game Programming</b><br>
-                &#x2022;Acquire new skills and techniques from classes and self-research<br>
-                &#x2022;Troubleshoot diligently to ensure quality<br>
-            </p>
-            <p>
-                <b>Cooking</b><br>
-                &#x2022;Measure precise ingredients to create unique dishes<br>
-                &#x2022;Estimate measurements when appropriate instruments are unavailable<br>
-            </p>
-        </article>
-    </div>
-</body>
-</html>
+<div class="resumes">
+    <article>
+        <h2>Education</h2>
+        <p>
+            <a class="resuLink" href="https://omds.ucsd.edu/" target="_blank"><b>University of California San Diego, Halicioğlu Data Science Institute</b></a><br><br>
+            &#x2022;Master of Data Science Online<br>
+            &#x2022;GPA 3.783<br>
+            &#x2022;Expected <time datetime="2024-03">March 2024</time>
+        </p>
+        <p><a class="resuLink" href="https://dornsife.usc.edu/cognitive-science/" target="_blank"><b>University of Southern California, Dornsife College of Letters, Arts and Sciences</b></a><br><br>
+            &#x2022;Bachelor of Arts in Cognitive Science<br>
+            &#x2022;Minor in Computer Programming<br>
+            &#x2022;GPA 3.45<br>
+            &#x2022;Graduated <time datetime="2022-05">May 2022</time></p>
+    </article>
+    <article>
+        <h2>Employment</h2>
+        <p>
+            <b><a class="resuLink" href="https://youtu.be/dQw4w9WgXcQ?si=yFpgaZFQPK23v-I4" target="_blank">Department of Defense, Data Analyst</b><br><br>
+            &#x2022;Collect and organize data from various sources and mediums<br>
+            &#x2022;Interact with stakeholders and present deliverables with clarity and brevity<br>
+            &#x2022;Allocate time and effort spent supporting different programs<br>
+            &#x2022;Employed since <time datetime="2022-09">September 2022</time>
+        </p>
+        <p>
+            <b><a class="resuLink" href="https://usctrojans.com/sports/womens-tennis" target="_blank">USC Athletics, Tennis Team Manager-USC Women's Tennis</b><br><br>
+            &#x2022;Communicate with the team to maintain a precise record of materials used<br>
+            &#x2022;Apply critical thinking and problem solving to overcome issues with varying product makeup<br>
+            &#x2022;Anticipate problems and communicate with the appropriate team member to deal with them<br>
+            &#x2022;Employed between <time datetime="2019-08">August 2019</time> - <time datetime="2022-05">May 2022</time>
+        </p>
+    </article>
+    <article>
+        <h2>Skills</h2>
+        <p>
+            &#x2022;<b>Languages:</b> Fluent in English; Conversational Proficiency in Spanish<br><br>
+            &#x2022;<b>Computer Skills:</b> Skilled in the fundamentals and the data structures of C++, Built web applications through HTML, CSS, and JavaScript with a database interface through PHP
+        </p>
+    </article>
+    <article>
+        <h2>Hobbies</h2>
+        <p>
+            <b>Video Game Programming</b><br>
+            &#x2022;Acquire new skills and techniques from classes and self-research<br>
+            &#x2022;Troubleshoot diligently to ensure quality<br>
+        </p>
+        <p>
+            <b>Cooking</b><br>
+            &#x2022;Measure precise ingredients to create unique dishes<br>
+            &#x2022;Estimate measurements when appropriate instruments are unavailable<br>
+        </p>
+    </article>
+</div>
