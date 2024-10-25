1d0620179d256db6e10ec05fea1f814837093cea
ntd002 <ntd002@ucsd.edu>
2024-10-25 08:57:50 -0700

    Start of Optional Content

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { page } from '$app/stores';
+    let pages = [
+    { url: './', title: 'Home' },
+    { url: './projects', title: 'Projects' },
+    { url: './contact', title: 'Contact' },
+    { url: './resume', title: 'Resumes' },
+    { url: './https://github.com/ntd002', title: 'Github Profile' },
+    ];
+</script>
+<nav>
+    {#each pages as p }
+        <a href={p.url}>{p.title}</a>
+    {/each}
+</nav>
+
+
