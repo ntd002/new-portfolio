87e01b6fe33fd4fd7e7f3afc3a2045426d06fd09
ntd002 <ntd002@ucsd.edu>
2024-11-06 19:59:33 -0800

    Added year

--- src/lib/Project.svelte
+++ src/lib/Project.svelte
@@ -6,5 +6,17 @@
 <article>
     <svelte:element this={'h' + hLevel}>{data.title}</svelte:element>   
     <img src={data.image} alt="">
-    <p>{data.description}</p>
+    <div>
+        <p>{data.description}</p>
+        <p class="year">c. {data.year}</p>
+    </div>
 </article>
+
+<style>
+    .year { 
+        font-weight: bold;
+        font-variant-numeric: oldstyle-nums;
+        color:#666666;
+    }
+
+</style>
