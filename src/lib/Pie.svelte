72bfd21e2683d24391a36cf13d90b9e483db1909
ntd002 <ntd002@ucsd.edu>
2024-11-06 20:14:15 -0800

    Step 1 Complete

---
+++ src/lib/Pie.svelte
@@ -0,0 +1,50 @@
+<script>
+    import * as d3 from 'd3';
+
+    let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+    let arc = arcGenerator({
+    startAngle: 0,
+    endAngle: 2 * Math.PI,
+    });
+
+    // let data = [1, 2];
+    // let total = 0;
+    // for (let d of data) {
+    // total += d;
+    // }
+
+    // let angle = 0;
+    // let arcData = [];
+    // for (let d of data) {
+    // let endAngle = angle + (d / total) * 2 * Math.PI;
+    // arcData.push({ startAngle: angle, endAngle });
+    // angle = endAngle;
+    // }
+
+    // let arcs = arcData.map((d) => arcGenerator(d));
+
+
+    let data = [1, 2, 3, 4, 5, 5];
+    let sliceGenerator = d3.pie();
+    let arcData = sliceGenerator(data);
+    let arcs = arcData.map((d) => arcGenerator(d));
+
+    let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+</script>
+
+<svg viewBox="-50 -50 100 100">
+    {#each arcs as arc, i}
+    <path d="{arc}" fill="{colors(i)}" />
+    {/each}
+</svg>
+
+<style>
+    svg{
+    max-width: 20em;
+    margin-block: 2em;
+
+    /* Do not clip shapes outside the viewBox */
+    overflow: visible;
+    }
+</style>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     import projects from '$lib/projects.json';
     import Project from '$lib/Project.svelte';
+    import Pie from '$lib/Pie.svelte';
 </script>
 
 <svelte:head>
@@ -8,6 +9,7 @@
 </svelte:head>
 
 <h1>{ projects.length } Projects</h1>
+<Pie/>
 <div class="projects">
     {#each projects as p}
     <article>
