1cbf5842b88a294360c14bb936bf0de3718b4d2b
ntd002 <ntd002@ucsd.edu>
2024-11-07 13:21:51 -0800

    Step 3 Complete

--- src/lib/Pie.svelte
+++ src/lib/Pie.svelte
@@ -23,15 +23,7 @@
 
     // let arcs = arcData.map((d) => arcGenerator(d));
 
-
-    let data = [
-    { value: 1, label: 'apples' },
-    { value: 2, label: 'oranges' },
-    { value: 3, label: 'mangos' },
-    { value: 4, label: 'pears' },
-    { value: 5, label: 'limes' },
-    { value: 5, label: 'cherries' },
-    ];
+    export let data = [];
 
     let sliceGenerator = d3.pie().value((d) => d.value);
     let arcData = sliceGenerator(data);
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,6 +2,16 @@
     import projects from '$lib/projects.json';
     import Project from '$lib/Project.svelte';
     import Pie from '$lib/Pie.svelte';
+    import * as d3 from 'd3';
+    
+    let rolledData = d3.rollups(
+    projects,
+    (v) => v.length,
+    (d) => d.year,
+    );
+
+    let pieData = rolledData.map(([year, count]) => {
+        return { value: count, label: year }; });
 </script>
 
 <svelte:head>
@@ -9,7 +19,7 @@
 </svelte:head>
 
 <h1>{ projects.length } Projects</h1>
-<Pie/>
+<Pie data="{pieData}"/>
 <div class="projects">
     {#each projects as p}
     <article>
